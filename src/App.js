3c84607f26ee5660786791d612451e85cea357b1
tesshsu <tesshsu@gmail.com>
2020-10-30 17:23:45 +0100

    feat: change contact filed

--- src/App.js
+++ src/App.js
@@ -10,17 +10,13 @@ import ContactForm from './components/contact'
 const config = {
   api: `http://localhost:3000/api/contact/index.php`,
   title: 'SI VOUS AVEZ BESOIN D’UN DÉVELOPPEUR EXPÉRIMENTÉ POUR VOTRE APPLICATION MOBILE, APPLICATION WEB E-COMMERCE, MARKETPLACE, UNE API OU SYSTEM ERP, CRM ETC., VOUS POUVEZ SOIT M’EMBAUCHER DIRECTEMENT EN UTILISANT LE FORMULAIRE CI-DESSOUS',
-  successMessage: 'Merci de me contacter.',
-  errorMessage: 'Désolé, nous avons des problèmes.',
+  successMessage: 'Merci de me contacter. On vous contacterai au sien de votre demande',
+  errorMessage: 'Désolé, nous avons des problèmes, vous pourriez également me contacter direct par email.',
   fields:{
-    firstName: '',
-    lastName: '',
     email: '',
     msg: ''
   },
   fieldsConfig:  [
-   { id: 1, label: 'Prénom', fieldName: 'Prénom', type: 'text',placeholder:'Votre Prénom', isRequired: true , klassName:'first-name-field'},
-   { id: 2, label: 'Nom', fieldName: 'Nom', type: 'text', placeholder: 'Votre Nom', isRequired: true , klassName:'last-name-field'},
    { id: 3, label: 'Email', fieldName: 'email', type: 'email', placeholder: ' Votre Email', isRequired: true , klassName:'email-field'},
    { id: 4, label: 'Message', fieldName: 'msg', type: 'textarea',placeholder:'Votre project.....', isRequired: true , klassName:'message-field'}
   ]
