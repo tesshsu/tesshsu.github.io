868ddfbe530b3ee8df21e449ba93c83f09f94ed9
yating <yating@itekcom.com>
2024-03-21 13:58:55 +0100

    Updates

---
+++ precache-manifest.da38647c049180120e7de61b404f54a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e716cdecd93331482a5",
+    "url": "/static/css/main.d96789bc.chunk.css"
+  },
+  {
+    "revision": "4e716cdecd93331482a5",
+    "url": "/static/js/main.4e716cde.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "e7ae0927d8118654e798",
+    "url": "/static/js/2.e7ae0927.chunk.js"
+  },
+  {
+    "revision": "3a7451befefd47c425c397e5e40d54f3",
+    "url": "/index.html"
+  }
+];
