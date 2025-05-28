d2fad8a5f23ed145f12af453036f518ccb36c4d7
tess hsu <tess.hsu@gmail.com>
2025-05-28 16:51:56 +0200

    feat-blog11

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -166,7 +166,7 @@ function setupBlogNavigation() {
 
     const prevLink = navBlock.querySelector('a:nth-child(1)');
     const nextLink = navBlock.querySelector('a:nth-child(2)');
-    const maxBlogNumber = 10;
+    const maxBlogNumber = 11;
 
     // Check if prevLink and nextLink exist
     if (!prevLink || !nextLink) {
